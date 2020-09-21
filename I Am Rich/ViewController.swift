0ccafd4339749477802e243f135c6cf77a24f39a
MaryTim <61231496+MaryTim@users.noreply.github.com>
2020-09-21 22:22:55 +0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Anton Budkevich on 9/21/20.
+//  Copyright © 2020 Mary Tim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
